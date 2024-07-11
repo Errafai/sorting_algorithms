bd7cc7b863acedaca7c0ff534d02fd533a10630f
Errafai <errafaiachraf1923@gmail.com>
2024-07-11 12:09:44 +0100

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,15 +11,17 @@ void bubble_sort(int *array, size_t size)
 		size_t j;
 		int tmp = 0;
 
-		for (i = 1; i < size; i++)
-				for (j = 0; j < i; j++)
+		for (i = 0; i < size - 1; i++)
+		{
+				for (j = i + 1; j < size; j++)
 				{
-						if (array[i] < array[j])
+						if (array[i] > array[j])
 						{
 								tmp = array[i];
 								array[i] = array[j];
 								array[j] = tmp;
+								print_array(array, size);
 						}
-						print_array(array, size);
 				}
+		}
 }
