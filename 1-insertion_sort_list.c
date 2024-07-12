4cec6d83c1a1701adb224abe636084c8df6871b1
Errafai <errafaiachraf1923@gmail.com>
2024-07-12 22:30:50 +0100

    1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - use insertion sort to sort a
+ *						doubly linked list.
+ *@list: the head of the list
+ *print at every swap the whole list
+ */
+
+
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *curr, *pcurr;
+
+	curr = (*list)->next;
+
+	while (curr != NULL)
+	{
+		pcurr = curr->prev;
+		while (pcurr != NULL && pcurr->n > curr->n)
+		{
+			if (pcurr->prev != NULL)
+				pcurr->prev->next = curr;
+			else
+				*list = curr;
+
+			if (curr->next != NULL)
+				curr->next->prev = pcurr;
+
+			curr->prev = pcurr->prev;
+			pcurr->next = curr->next;
+			curr->next = pcurr;
+			pcurr->prev = curr;
+			print_list(*list);
+
+			pcurr = curr->prev;
+		}
+		curr = curr->next;
+	}
+}
